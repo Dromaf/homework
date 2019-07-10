935f79c64affc4840bf0a3fe3c98cbc02ab0b13f
Dromaf <pdvxai@gmail.com>
2019-07-10 20:38:06 +0300

    js task

---
+++ lesson17/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+/* TASK1 */
+
+const t = 3, m = 60, s = 60;
+
+let result = t * m * s;
+    
+console.log(`В ${t}-х часах: ${result} секунд`);
+
+/* TASK2 */
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num += 1;
+num -= 1;
+num++;
+num--;
+alert(num);
+
+/* TASK3 */
+
+let a=10, b=2, c=5;
+
+console.log(a + b + c);
+
+let d = a + b + c;
+
+console.log(d);
+
+/* TASK4 */
+
+a = 17, b = 10;
+
+c = a - b;
+
+d = 7;
+
+result = c + d;
+
+console.log(result);
+/* TASK5 */
+
+let name = "Dmitriy";
+
+console.log(`Привет, ${name}!`);
+console.log("Привет," + " " + name + "!");
+console.log('Привет,' + ' ' + name + '!');
+
