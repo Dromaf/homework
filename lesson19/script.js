295a38f6ea2c5d431380c197fe7fddef7d18c417
Dromaf <pdvxai@gmail.com>
2019-07-17 21:57:44 +0300

    fix

--- lesson19/script.js
+++ lesson19/script.js
@@ -1,16 +1,21 @@
 "use strict";
 
-/*TASK1*/
+/*1. Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно 
+больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.*/
+
 function isNumberInRange(a) {
     if (a > 0 && a < 10){
         console.log(true);
     } else {
         console.log(false);
     }
-    return
+    return;
 }
-isNumberInRange(prompt("Ввведите число", ""));
-/*TASK2*/
+isNumberInRange(+prompt("Ввведите число", ""));
+
+/*2. Сделайте функцию isEven() (even - это четный), которая параметром принимает целое число 
+и проверяет: четное оно или нет. Если четное - пусть функция возвращает true, если нечетное - false.*/
+
 function isEven(even) {
     if (even%2 == 0) {
         console.log(true);
@@ -18,8 +23,22 @@ function isEven(even) {
         console.log(false);
     }
 }
-isEven(prompt("Ввведите число", ""));
-/*TASK3*/
+isEven(+prompt("Ввведите число", ""));
+
+/*3. Переписать код из конструкции if в switch
+
+var value = +prompt('value?', '');
+if (value == 0) {
+  alert( 0 );
+}
+if (value == 1) {
+  alert( 1 );
+}
+if (value == 2 || value == 3) {
+  alert( '2,3' );
+}
+*/
+
 let value = +prompt("value?", "");
 
 switch(value){
@@ -36,8 +55,12 @@ switch(value){
     default:
         console.log('Я таких значений не знаю');
 }
-/*TASK4*/
 
+/*4. Напишите функцию min(a,b), которая возвращает меньшее из чисел a,b
+
+Пример вызовов: 
+min(3, 5) // вернет 3
+min(5, -1) // вернет -1*/
 
 function min(aa, bb){
   
