5d0c9ca3e9486a5402fd6406aea6e2c45ec8ff9e
Dromaf <pdvxai@gmail.com>
2019-07-31 20:45:10 +0300

    day

---
+++ lesson23/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/* TASK 1 */
+let time = new Date();
+
+let getHours = 24 - time.getHours();
+let minutes =  60 - time.getMinutes();
+let seconds =  60 - time.getSeconds();
+
+console.log(`До конца дня осталось ${getHours} : ${minutes} : ${seconds}` );
+
+/* TASK 2 */
+
+let time1 = new Date();
+let time2 = new Date(time1.getFullYear(), 0, 1, 0, 0);
+
+let res = Math.round((time1-time2) / 1000);
+
+console.log(res);
+
+/* TASK 3 */
+
+function getWeekDay(date) {
+  let days = ['Воскресени', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+  return days[date.getDay()];
+}
+let date = new Date(2013, 0, 25); 
+console.log( getWeekDay(date) ); 
+
+/* TASK 4 */
+
+
+function daysOst() { 
+  let dnuxa = prompt('Пожалуйста введите дату вашего рождения в формате YYYY-MM-DD', '');
+
+  let birthday = new Date(Date.parse(dnuxa));
+  let today = new Date();
+  let nDate = birthday.setFullYear(today.getFullYear());
+  if (nDate < today) {
+    birthday.setFullYear(today.getFullYear()+1);
+  }
+  let allSec = 24*60*60*1000; 
+  let dayswait = Math.round((birthday.getTime() - today.getTime())/allSec);
+  
+  console.log(`До вашего дня рождения осталось ${dayswait} дней`);
+  }
+
+  daysOst();
